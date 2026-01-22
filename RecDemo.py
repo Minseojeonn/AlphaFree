74bd4b09f1fe260b07802f132fc391906d52967c
raichal2000@naver.com <raichal2000@naver.com>
2026-01-22 09:22:05 +0000

    fix: error

--- RecDemo.py
+++ RecDemo.py
@@ -1,5 +1,5 @@
-from AlphaFree.models.AlphaFreeRecDemo import AlphaFreeRecDemo
-from parse import parse_args
+from models.AlphaFreeRecDemo import AlphaFreeRecDemo
+from .models.base.parse import parse_args
 import gdown
 import torch
 import os
