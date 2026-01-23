b3d3c907e219a4e79d655697cf9cc8c7529454e7
miseo_jeon <83704225+Minseojeonn@users.noreply.github.com>
2026-01-23 15:53:24 +0900

    Update download file IDs in download.sh
    Updated file IDs for downloads in download.sh.

--- data/download.sh
+++ data/download.sh
@@ -6,13 +6,13 @@ if ! command -v gdown &> /dev/null; then
 fi
 
 declare -A FILES
-FILES["steam.zip"]="1pUaW4cl56VpwtH6ICCKn_HQUlqqN71KL"
-FILES["amazon_movie.zip"]="10P4C2DF8XqJVkxQlkA2kqzzG1ZNdkrgZ"
-FILES["amazon_book_2014.zip"]="1oHc-5aFqJnD9U2__r-gpfmV-k0dzMJYK"
-FILES["amazon_video.zip"]="1BUm2k4Yn2oPLgQTzLvQncgPZqpvmCT7j"
-FILES["amazon_baby.zip"]="1D3ukli7JKVY5zv5Km3Q8bvkaTEt0KBUJ"
-FILES["amazon_beauty_personal.zip"]="1TY4iWVJQZ1DgVc74ZwQz5k1aRrxfDW0Z"
-FILES["amazon_health.zip"]="1ZBryoZ6rO6lQ9fID8Fw1_xD-qTW4187y"
+FILES["steam.zip"]="1Ck5wx2wTxFFaUoZNSG8dA-pyHEMD1ryA"
+FILES["amazon_movie.zip"]="169UHsoOElhwL1LkYnBCpi6kdJ0hmFBVV"
+FILES["amazon_book_2014.zip"]="1PkD5sT7yy8qDNqUSKT8wkZZYUL9U4u5A"
+FILES["amazon_video.zip"]="1rYbqJPjvHhac92Gx2A25tHtwWKCuYsIe"
+FILES["amazon_baby.zip"]="18NmIghILo7EKYD94DE0SEl7M8YnanElD"
+FILES["amazon_beauty_personal.zip"]="1M_ybwPrYxseYZwhzxevvCP0zPQO2gJEA"
+FILES["amazon_health.zip"]="1LFton2fZx9yDcTBsIB9pmiHyGKFuU4re"
 echo "Select a file to download:"
 select NAME in "${!FILES[@]}"; do
     if [[ -n "$NAME" ]]; then
